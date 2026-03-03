23e5579cfb859c3b909206c036cb689f0c7c57e1
guillem.pages.bustamante <guillem.pages.bustamante@estudiantat.upc.edu>
2026-03-03 18:21:59 +0100

    clase calculadora 2

--- Calculadora.py
+++ Calculadora.py
@@ -6,3 +6,15 @@ def multiplicacion(a,b):
     return a*b
 def division(a,b):
     return a/b
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    suma = suma(2,3)
+    print(suma)
+    resta = resta(3,4)
+    print(resta)
+    multiplicacion = multiplicacion(3,4)
+    print(multiplicacion)
+    division = division(3,4)
+    print(division)
